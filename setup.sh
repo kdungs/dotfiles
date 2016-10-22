ddea5b463e9d9e2a1507e97700e89ae53211a6d4
Kevin Dungs <kevin@dun.gs>
2016-10-22 22:12:03 +0200

    Add tmux config.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh-plugins ~/.zsh-plugins
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
