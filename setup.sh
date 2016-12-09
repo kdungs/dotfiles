c3859f8b06017750bd8e1a9e7d9b501516e58572
Kevin Dungs <kevin@dun.gs>
2016-12-09 17:40:11 +0100

    Add git configuration.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/zshrc ~/.zshrc
